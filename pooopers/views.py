190ab6ac497386f505020a40c915f269fc0b1a79
Fabio Rueda <avances123@gmail.com>
2015-09-04 18:16:04 +0200

    pooopers/admin/reset para borrar todas las cagadas de alguien

--- pooopers/views.py
+++ pooopers/views.py
@@ -2,9 +2,20 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from pooopers.models import Poooper
 from pooopers.serializers import PoooperSerializer
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class PoooperViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Poooper.objects.all()
     serializer_class = PoooperSerializer
+
+    @detail_route(methods=['post','get'],url_path='reset')
+    def reset(self,request,pk=None):
+        user = self.get_object()
+        user.pooops.all().delete()
+        return Response(None,status=status.HTTP_204_NO_CONTENT)
+
